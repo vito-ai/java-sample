13df3ad69eb4c958e7d5051a0dcd403e68929ba9
Homura <homura@rtzr.ai>
2023-08-25 18:46:15 +0900

    fix okhttp3 send buffer full bug

--- src/main/java/ai/vito/openapi/stream/VitoSttWebSocketClient.java
+++ src/main/java/ai/vito/openapi/stream/VitoSttWebSocketClient.java
@@ -3,6 +3,7 @@ package ai.vito.openapi.stream;
 import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.concurrent.CountDownLatch;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -17,52 +18,62 @@ import okhttp3.WebSocketListener;
 import okio.ByteString;
 
 public class VitoSttWebSocketClient {
-	
-	public static void main(String[] args) throws Exception {
-		OkHttpClient client = new OkHttpClient();
 
-		String token = Auth.getAccessToken();
-		
-		HttpUrl.Builder httpBuilder = HttpUrl.get("https://openapi.vito.ai/v1/transcribe:streaming").newBuilder();
-	    httpBuilder.addQueryParameter("sample_rate","8000");
-	    httpBuilder.addQueryParameter("encoding","LINEAR16");
-	    httpBuilder.addQueryParameter("use_itn","true");
-	    httpBuilder.addQueryParameter("use_disfluency_filter","true");
-	    httpBuilder.addQueryParameter("use_profanity_filter","true");
-		
-	    String url = httpBuilder.toString().replace("https://", "wss://");
+    public static void main(String[] args) throws Exception {
+        Logger logger = Logger.getLogger(VitoSttWebSocketClient.class.getName());
+        OkHttpClient client = new OkHttpClient();
+
+        String token = Auth.getAccessToken();
+
+        HttpUrl.Builder httpBuilder = HttpUrl.get("https://openapi.vito.ai/v1/transcribe:streaming").newBuilder();
+        httpBuilder.addQueryParameter("sample_rate", "8000");
+        httpBuilder.addQueryParameter("encoding", "LINEAR16");
+        httpBuilder.addQueryParameter("use_itn", "true");
+        httpBuilder.addQueryParameter("use_disfluency_filter", "true");
+        httpBuilder.addQueryParameter("use_profanity_filter", "true");
+
+        String url = httpBuilder.toString().replace("https://", "wss://");
 
         Request request = new Request.Builder()
                 .url(url)
-                .addHeader("Authorization","Bearer "+token)
+                .addHeader("Authorization", "Bearer " + token)
                 .build();
 
         VitoWebSocketListener webSocketListener = new VitoWebSocketListener();
         WebSocket vitoWebSocket = client.newWebSocket(request, webSocketListener);
-        
+
         File file = new File("sample.wav");
-		AudioInputStream in = AudioSystem.getAudioInputStream(file);
-		
+        AudioInputStream in = AudioSystem.getAudioInputStream(file);
+
         byte[] buffer = new byte[1024];
-        while (in.read(buffer) != -1) {
-        	vitoWebSocket.send(ByteString.of(buffer));
+        int readBytes;
+        while ((readBytes = in.read(buffer)) != -1) {
+            boolean sent = vitoWebSocket.send(ByteString.of(buffer, 0, readBytes));
+            if (!sent) {
+                logger.log(Level.WARNING, "Send buffer is full. Cannot complete request. Increase sleep interval.");
+                System.exit(1);
+            }
+            Thread.sleep(0, 50);
         }
-        
+        vitoWebSocket.send("EOS");
+
+        webSocketListener.waitClose();
         client.dispatcher().executorService().shutdown();
-	}
+    }
 }
 
 class VitoWebSocketListener extends WebSocketListener {
-	private static final Logger logger = Logger.getLogger(VitoSttGrpcClient.class.getName());
-	private static final int NORMAL_CLOSURE_STATUS = 1000;
-	
-	private static void log(Level level, String msg, Object... args) {
+    private static final Logger logger = Logger.getLogger(VitoSttWebSocketClient.class.getName());
+    private static final int NORMAL_CLOSURE_STATUS = 1000;
+    private final CountDownLatch latch = new CountDownLatch(1);
+
+    private static void log(Level level, String msg, Object... args) {
         logger.log(level, msg, args);
     }
 
     @Override
     public void onOpen(WebSocket webSocket, Response response) {
-    	log(Level.INFO, "Open " + response.message());
+        log(Level.INFO, "Open " + response.message());
     }
 
     @Override
@@ -72,19 +83,30 @@ class VitoWebSocketListener extends WebSocketListener {
 
     @Override
     public void onMessage(WebSocket webSocket, ByteString bytes) {
-    	System.out.println(bytes.hex());
+        System.out.println(bytes.hex());
     }
 
     @Override
     public void onClosing(WebSocket webSocket, int code, String reason) {
         webSocket.close(NORMAL_CLOSURE_STATUS, null);
-        log(Level.INFO, "Closing", code, reason);
+        log(Level.INFO, "Closing {0} {1}", code, reason);
+    }
+
+    @Override
+    public void onClosed(WebSocket webSocket, int code, String reason) {
+        webSocket.close(NORMAL_CLOSURE_STATUS, null);
+        log(Level.INFO, "Closed {0} {1}", code, reason);
+        latch.countDown();
     }
 
     @Override
     public void onFailure(WebSocket webSocket, Throwable t, Response response) {
         t.printStackTrace();
-        
+        latch.countDown();
     }
-	
+
+    public void waitClose() throws InterruptedException {
+        log(Level.INFO, "Wait for finish");
+        latch.await();
+    }
 }
