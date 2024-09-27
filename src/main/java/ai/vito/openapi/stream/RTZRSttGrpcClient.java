def9faad0f7517db831f49e921959f61b13690c8
Sungyoun Kim(Peter) <58387861+Sungyoun-Kim@users.noreply.github.com>
2024-09-27 20:56:25 +0900

    S2-446 읽은 바이트만큼 sleep하도록 수정 (#2)
    * 읽은 바이트만큼 sleep하도록 수정
    
    * 지원 파일 포맷 주석 추가
    
    * 주석추가 및 하드코딩된 값 변수로 치환
    
    * sleep 연산식에 괄호 추가
    
    * vito -> rtzr로 이름 변경
    
    * 기존에 있던 sleep 제거

--- src/main/java/ai/vito/openapi/stream/RTZRSttGrpcClient.java
+++ src/main/java/ai/vito/openapi/stream/RTZRSttGrpcClient.java
@@ -1,3 +1,8 @@
+/* 해당 예제는 아래 파일 포맷을 지원합니다
+WAV, AU, AIFF
+https://docs.oracle.com/javase/8/docs/technotes/guides/sound/index.html
+*/
+
 package ai.vito.openapi.stream;
 
 import ai.vito.openapi.auth.Auth;
@@ -8,30 +13,77 @@ import com.google.protobuf.ByteString;
 import io.grpc.*;
 import io.grpc.stub.StreamObserver;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
 import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.sound.sampled.*;
 
-public class VitoSttGrpcClient {
-    private static final Logger logger = Logger.getLogger(VitoSttGrpcClient.class.getName());
+/*
+본 예제에서는 스트리밍 입력을 음성파일을 읽어서 시뮬레이션 합니다.
+실제사용시에는 마이크 입력 등의 실시간 음성 스트림이 들어와야합니다.
+*/
+final class FileStreamer {
+    private AudioInputStream audio8KStream;
+    private int SAMPLE_RATE = 8000;
+    private int BITS_PER_SAMPLE = 16;
+
+    public FileStreamer(String filePath) throws IOException, UnsupportedAudioFileException {
+        File file = new File(filePath);
+        try {
+            AudioInputStream originalAudioStream = AudioSystem.getAudioInputStream(file);
+            AudioFormat originalFormat = originalAudioStream.getFormat();
+            AudioFormat newFormat = new AudioFormat(
+                    AudioFormat.Encoding.PCM_SIGNED,
+                    SAMPLE_RATE,
+                    BITS_PER_SAMPLE,
+                    1,
+                    1 * (BITS_PER_SAMPLE / 8),
+                    SAMPLE_RATE,
+                    originalFormat.isBigEndian());
+
+            this.audio8KStream = AudioSystem.getAudioInputStream(newFormat, originalAudioStream);
+        } catch (IOException | UnsupportedAudioFileException e) {
+            throw e;
+        }
+    }
+
+    public int read(byte[] b) throws IOException, InterruptedException {
+        int maxSize = 1024 * 1024;
+        int byteSize = Math.min(b.length, maxSize);
+        try {
+            Thread.sleep(byteSize / ((SAMPLE_RATE * (BITS_PER_SAMPLE / 8)) / 1000));
+        } catch (InterruptedException e) {
+            throw e;
+        }
+        return this.audio8KStream.read(b, 0, byteSize);
+    }
+
+    public void close() throws IOException {
+        this.audio8KStream.close();
+    }
+}
+
+public class RTZRSttGrpcClient {
+    private static final Logger logger = Logger.getLogger(RTZRSttGrpcClient.class.getName());
 
     private final OnlineDecoderGrpc.OnlineDecoderStub asyncStub;
     private final StreamObserver<DecoderRequest> decoder;
     private final CountDownLatch finishLatch;
 
-    public VitoSttGrpcClient(Channel channel, final String token, final StreamObserver<DecoderResponse> observer) {
+    public RTZRSttGrpcClient(Channel channel, final String token, final StreamObserver<DecoderResponse> observer) {
         finishLatch = new CountDownLatch(1);
         asyncStub = OnlineDecoderGrpc.newStub(channel)
                 .withCallCredentials(new CallCredentials() {
                     @Override
-                    public void applyRequestMetadata(RequestInfo requestInfo, Executor appExecutor, MetadataApplier applier) {
+                    public void applyRequestMetadata(RequestInfo requestInfo, Executor appExecutor,
+                            MetadataApplier applier) {
                         final Metadata metadata = new Metadata();
-                        metadata.put(Metadata.Key.of("authorization", Metadata.ASCII_STRING_MARSHALLER), "Bearer " + token);
+                        metadata.put(Metadata.Key.of("authorization", Metadata.ASCII_STRING_MARSHALLER),
+                                "Bearer " + token);
                         applier.apply(metadata);
                     }
 
@@ -60,7 +112,6 @@ public class VitoSttGrpcClient {
         });
     }
 
-
     public void await(long timeout, TimeUnit unit) throws InterruptedException {
         finishLatch.await(timeout, unit);
     }
@@ -84,16 +135,20 @@ public class VitoSttGrpcClient {
     private static void log(Level level, String msg, Object... args) {
         logger.log(level, msg, args);
     }
-    
+
+    private static void log(Level level, String msg, Throwable t) {
+        logger.log(level, msg, t);
+    }
+
     public static void main(String[] args) throws Exception {
 
         ManagedChannel channel = ManagedChannelBuilder.forTarget("grpc-openapi.vito.ai:443")
                 .useTransportSecurity()
                 .build();
-        
+
         String token = Auth.getAccessToken();
-        
-        VitoSttGrpcClient client = new VitoSttGrpcClient(channel, token, new StreamObserver<DecoderResponse>() {
+
+        RTZRSttGrpcClient client = new RTZRSttGrpcClient(channel, token, new StreamObserver<DecoderResponse>() {
             @Override
             public void onNext(DecoderResponse value) {
                 StreamingRecognitionResult result = value.getResults(0);
@@ -101,9 +156,10 @@ public class VitoSttGrpcClient {
                 if (result.getIsFinal()) {
                     System.out.printf("final:%6d,%6d: %s\n", result.getStartAt(), result.getDuration(), best.getText());
                 } else {
-                    System.out.printf(best.getText() + "\n") ;
+                    System.out.printf(best.getText() + "\n");
                 }
             }
+
             @Override
             public void onError(Throwable t) {
                 log(Level.WARNING, "on error", t);
@@ -114,22 +170,21 @@ public class VitoSttGrpcClient {
                 log(Level.INFO, "Complete");
             }
         });
-        File file = new File("sample.wav");
-        AudioInputStream in = AudioSystem.getAudioInputStream(file);
-        DecoderConfig config = DecoderConfig.newBuilder().
-                setSampleRate(8000).
-                setEncoding(DecoderConfig.AudioEncoding.LINEAR16).
-                setUseItn(true).
-                setUseDisfluencyFilter(true).
-                setUseProfanityFilter(true).build();
+        FileStreamer fileStreamer = new FileStreamer(
+                "sample.wav");
+
+        DecoderConfig config = DecoderConfig.newBuilder().setSampleRate(8000)
+                .setEncoding(DecoderConfig.AudioEncoding.LINEAR16).setUseItn(true).setUseDisfluencyFilter(true)
+                .setUseProfanityFilter(true).build();
 
         client.setDecoderConfig(config);
         byte[] buffer = new byte[1024];
-        int readed = 0;
+        int readBytes = 0;
         // Try to read numBytes bytes from the file.
-        while ((readed = in.read(buffer)) != -1) {
-            client.send(buffer, readed);
+        while ((readBytes = fileStreamer.read(buffer)) != -1) {
+            client.send(buffer, readBytes);
         }
+        fileStreamer.close();
         client.closeSend();
         client.await();
     }
--- src/main/java/ai/vito/openapi/stream/RTZRSttWebSocketClient.java
+++ src/main/java/ai/vito/openapi/stream/RTZRSttWebSocketClient.java
@@ -1,13 +1,20 @@
+/* 해당 예제는 아래 파일 포맷을 지원합니다
+WAV, AU, AIFF
+https://docs.oracle.com/javase/8/docs/technotes/guides/sound/index.html
+*/
+
 package ai.vito.openapi.stream;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.concurrent.CountDownLatch;
 
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 import ai.vito.openapi.auth.Auth;
 import okhttp3.HttpUrl;
@@ -18,10 +25,55 @@ import okhttp3.WebSocket;
 import okhttp3.WebSocketListener;
 import okio.ByteString;
 
-public class VitoSttWebSocketClient {
+/*
+본 예제에서는 스트리밍 입력을 음성파일을 읽어서 시뮬레이션 합니다.
+실제사용시에는 마이크 입력 등의 실시간 음성 스트림이 들어와야합니다.
+*/
+final class FileStreamer {
+    private AudioInputStream audio8KStream;
+    private int SAMPLE_RATE = 8000;
+    private int BITS_PER_SAMPLE = 16;
+
+    public FileStreamer(String filePath) throws IOException, UnsupportedAudioFileException {
+        File file = new File(filePath);
+        try {
+            AudioInputStream originalAudioStream = AudioSystem.getAudioInputStream(file);
+            AudioFormat originalFormat = originalAudioStream.getFormat();
+            AudioFormat newFormat = new AudioFormat(
+                    AudioFormat.Encoding.PCM_SIGNED,
+                    SAMPLE_RATE,
+                    BITS_PER_SAMPLE,
+                    1,
+                    1 * (BITS_PER_SAMPLE / 8),
+                    SAMPLE_RATE,
+                    originalFormat.isBigEndian());
+
+            this.audio8KStream = AudioSystem.getAudioInputStream(newFormat, originalAudioStream);
+        } catch (IOException | UnsupportedAudioFileException e) {
+            throw e;
+        }
+    }
+
+    public int read(byte[] b) throws IOException, InterruptedException {
+        int maxSize = 1024 * 1024;
+        int byteSize = Math.min(b.length, maxSize);
+        try {
+            Thread.sleep(byteSize / ((SAMPLE_RATE * (BITS_PER_SAMPLE / 8)) / 1000));
+        } catch (InterruptedException e) {
+            throw e;
+        }
+        return this.audio8KStream.read(b, 0, byteSize);
+    }
+
+    public void close() throws IOException {
+        this.audio8KStream.close();
+    }
+}
+
+public class RTZRSttWebSocketClient {
 
     public static void main(String[] args) throws Exception {
-        Logger logger = Logger.getLogger(VitoSttWebSocketClient.class.getName());
+        Logger logger = Logger.getLogger(RTZRSttWebSocketClient.class.getName());
         OkHttpClient client = new OkHttpClient();
 
         String token = Auth.getAccessToken();
@@ -40,38 +92,30 @@ public class VitoSttWebSocketClient {
                 .addHeader("Authorization", "Bearer " + token)
                 .build();
 
-        VitoWebSocketListener webSocketListener = new VitoWebSocketListener();
-        WebSocket vitoWebSocket = client.newWebSocket(request, webSocketListener);
+        RTZRWebSocketListener webSocketListener = new RTZRWebSocketListener();
+        WebSocket rtzrWebSocket = client.newWebSocket(request, webSocketListener);
 
-        AudioInputStream in = null;
-        try {
-            File file = new File("sample.wav");
-            in = AudioSystem.getAudioInputStream(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
+        FileStreamer fileStreamer = new FileStreamer("sample.wav");
 
         byte[] buffer = new byte[1024];
         int readBytes;
-        while ((readBytes = in.read(buffer)) != -1) {
-            boolean sent = vitoWebSocket.send(ByteString.of(buffer, 0, readBytes));
+        while ((readBytes = fileStreamer.read(buffer)) != -1) {
+            boolean sent = rtzrWebSocket.send(ByteString.of(buffer, 0, readBytes));
             if (!sent) {
                 logger.log(Level.WARNING, "Send buffer is full. Cannot complete request. Increase sleep interval.");
                 System.exit(1);
             }
-            Thread.sleep(0, 50);
         }
-        in.close();
-        vitoWebSocket.send("EOS");
+        fileStreamer.close();
+        rtzrWebSocket.send("EOS");
 
         webSocketListener.waitClose();
         client.dispatcher().executorService().shutdown();
     }
 }
 
-class VitoWebSocketListener extends WebSocketListener {
-    private static final Logger logger = Logger.getLogger(VitoSttWebSocketClient.class.getName());
+class RTZRWebSocketListener extends WebSocketListener {
+    private static final Logger logger = Logger.getLogger(RTZRSttWebSocketClient.class.getName());
     private static final int NORMAL_CLOSURE_STATUS = 1000;
     private CountDownLatch latch = null;
 
